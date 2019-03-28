5ab9b16e40dd7adbc7a97da63cd8fbae7276b3e9
Alvaro <Alvaro@DESKTOP-TEHNTE9>
2019-03-28 17:29:06 +0100

    v2

---
+++ CalculatorService.Client/CalculatorService.Client/Result/DivRes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorService.Client
+{
+    class DivRes
+    {
+        public int Quotient;
+        public int Remainder;
+        
+    }
+}
---
+++ MVCalculator/MVCalculator/Result/AddRes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorService.Client
+{
+    class AddRes
+    {
+        public int Sum;
+        
+    }
+}
