85efcc429eb9bb1362ebb3d5a1ecc1482e285b6a
agomez97 <48960676+agomez97@users.noreply.github.com>
2019-03-28 15:03:13 +0100

    Add files via upload

---
+++ MVCalculator/MVCalculator/Operations/Sub.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorService.Client
+{
+    class Sub
+    {  
+        public int Minuend;
+        public int Substraend;
+        
+    }
+}
